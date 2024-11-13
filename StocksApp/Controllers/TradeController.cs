6bff129ab12fadd5ff3d84fa857e16e1f0d9bb23
EMonfort <emonfort@fi.uba.ar>
2024-11-12 22:54:50 -0300

    Add controller and view, with layout and view start

---
+++ StocksApp/Controllers/TradeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksApp.Controllers
+{
+    public class TradeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
