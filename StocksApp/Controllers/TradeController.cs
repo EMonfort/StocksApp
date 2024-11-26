cf22936aaddc81b3cb0da37cd757725d45a0debf
EMonfort <emonfort@fi.uba.ar>
2024-11-25 22:25:36 -0300

    Implement Controller

--- StocksApp/Controllers/TradeController.cs
+++ StocksApp/Controllers/TradeController.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ServiceContracts;
+using StocksApp.Models;
 
 namespace StocksApp.Controllers
 {
     public class TradeController : Controller
     {
+        private readonly TradingOptions _tradingOptions;
+        private readonly IFinnhubService _finnhubService;
+        private readonly IConfiguration _configuration;
+
+        public TradeController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _finnhubService = finnhubService;
+            _tradingOptions = tradingOptions.Value;
+        }
 
         [Route("/")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            if (string.IsNullOrEmpty(_tradingOptions.DefaultStockSymbol))
+                _tradingOptions.DefaultStockSymbol = "MSFT";
+
+            Dictionary<string, object>? companyProfileDictionary = await _finnhubService.GetCompanyProfile(_tradingOptions.DefaultStockSymbol);
+
+            Dictionary<string, object>? stockQuoteDictionary = await _finnhubService.GetStockPriceQuote(_tradingOptions.DefaultStockSymbol);
+
+            StockTrade stockTrade = new StockTrade() { StockSymbol = _tradingOptions.DefaultStockSymbol };
+
+            //load data from finnHubService into model object
+            if (companyProfileDictionary != null && stockQuoteDictionary != null)
+            {
+                stockTrade = new StockTrade() { StockSymbol = Convert.ToString(companyProfileDictionary["ticker"]), StockName = Convert.ToString(companyProfileDictionary["name"]), Price = Convert.ToDouble(stockQuoteDictionary["c"].ToString()) };
+            }
+
+            //Send Finnhub token to view
+            ViewBag.FinnhubToken = _configuration["FinnhubToken"];
+
+            return View(stockTrade);
         }
     }
 }
--- StocksApp/Program.cs
+++ StocksApp/Program.cs
@@ -1,9 +1,10 @@
 using ServiceContracts;
 using Services;
+using StocksApp;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IFinnhubService, FinnhubService>();
 
