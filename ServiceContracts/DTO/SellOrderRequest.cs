ae2f65d036556a3a07378b6457fb1bf093d3d71b
EMonfort <emonfort@fi.uba.ar>
2024-12-16 08:54:14 -0300

    Add DTOs

---
+++ ServiceContracts/DTO/SellOrderRequest.cs
@@ -0,0 +1,28 @@
+﻿using ServiceContracts.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    public class SellOrderRequest
+    {
+        [Required(ErrorMessage = "Stock Symbol can't be blank")]
+        public string StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "Stock Name can't be blank")]
+        public string StockName { get; set; }
+
+        [MinimumDateAttributeValidator]
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Quantity must be between {1} and {2}")]
+        public uint Quantity { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "Quantity must be between {1} and {2}")]
+        public double Price { get; set; }
+    }
+}
---
+++ ServiceContracts/Validators/MinimumDateAttributeValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.Validators
+{
+    public class MinimumDateAttributeValidator : ValidationAttribute
+    {
+        public DateTime MinimumDate { get; set; }
+
+        public string DefaultMessage { get; set; } = "Date should be newer than {0}";
+
+        public MinimumDateAttributeValidator()
+        {
+            
+        }
+
+        public MinimumDateAttributeValidator(string date)
+        {
+            MinimumDate = Convert.ToDateTime(date);
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null) 
+            { 
+                DateTime date = (DateTime)value;
+
+                if (date < MinimumDate)
+                {
+                    return new ValidationResult(String.Format(ErrorMessage ?? DefaultMessage, MinimumDate));
+                }
+                return ValidationResult.Success;
+            }
+            return null;
+        }
+    }
+}
