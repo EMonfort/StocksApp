5ccdd3caf844f5f963b05091613655964d088883
EMonfort <emonfort@fi.uba.ar>
2025-01-22 17:19:07 -0300

    Add Tag Helpers

--- ServiceContracts/DTO/SellOrderRequest.cs
+++ ServiceContracts/DTO/SellOrderRequest.cs
@@ -18,7 +18,7 @@ namespace ServiceContracts.DTO
         [Range(1, 100000, ErrorMessage = "Quantity must be between {1} and {2}")]
         public uint Quantity { get; set; }
 
-        [Range(1, 10000, ErrorMessage = "Quantity must be between {1} and {2}")]
+        [Range(1, 10000, ErrorMessage = "Price must be between {1} and {2}")]
         public double Price { get; set; }
 
         public SellOrder ToSellOrder()
